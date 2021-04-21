c044c47254cbc254f3496b5c22667bbbeb0221ab
WonYoung Choi <wy80.choi@samsung.com>
2021-04-21 09:55:48 +0900

    Add Samsung.Tizen.Templates package

---
+++ workload/src/Samsung.Tizen.Templates/tizen/Main.cs
@@ -0,0 +1,51 @@
+using System;
+
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace TizenApp1
+{
+    class Program : NUIApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            Window.Instance.KeyEvent += OnKeyEvent;
+
+            TextLabel text = new TextLabel("Hello Tizen") {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                TextColor = Color.Blue,
+                PointSize = 12.0f,
+                HeightResizePolicy = ResizePolicyType.FillToParent,
+                WidthResizePolicy = ResizePolicyType.FillToParent
+            };
+            Window.Instance.GetDefaultLayer().Add(text);
+
+            Animation animation = new Animation(2000);
+            animation.AnimateTo(text, "Orientation", new Rotation(new Radian(new Degree(180.0f)), PositionAxis.X), 0, 500);
+            animation.AnimateTo(text, "Orientation", new Rotation(new Radian(new Degree(0.0f)), PositionAxis.X), 500, 1000);
+            animation.Looping = true;
+            animation.Play();
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
