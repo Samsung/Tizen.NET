9ad1412b0453d6d6bf7b5489a2fc63b2ef43f280
김성수/Common Platform Lab(SR)/삼성전자 <sung-su.kim@samsung.com>
2022-07-12 11:49:12 +0900

    Add check workload installed

--- workload/scripts/workload-install.ps1
+++ workload/scripts/workload-install.ps1
@@ -195,16 +195,22 @@ Ensure-Directory $ManifestDir
 if (Test-Path $TizenManifestFile) {
     $ManifestJson = $(Get-Content $TizenManifestFile | ConvertFrom-Json)
     $OldVersion = $ManifestJson.version
-    if ($OldVersion -ne $Version) {
-        Write-Host "Removing $ManifestName/$OldVersion from $ManifestDir..."
-        Remove-Pack -Id $ManifestName -Version $OldVersion -Kind "manifest"
-        $ManifestJson.packs.PSObject.Properties | ForEach-Object {
-            Write-Host "Removing $($_.Name)/$($_.Value.version)..."
-            Remove-Pack -Id $_.Name -Version $_.Value.version -Kind $_.Value.kind
+    if ($OldVersion -eq $Version) {
+        $DotnetWorkloadList = Invoke-Expression "& '$DotnetCommand' workload list"
+        foreach ($Item in $DotnetWorkloadList)
+        {
+            if ($Item.StartsWith("tizen"))
+            {
+                Write-Host "$Version version is already installed."
+                Exit 0
+            }
         }
-    } else {
-        Write-Host "$Version version is already installed."
-        Exit 0
+    }
+    Write-Host "Removing $ManifestName/$OldVersion from $ManifestDir..."
+    Remove-Pack -Id $ManifestName -Version $OldVersion -Kind "manifest"
+    $ManifestJson.packs.PSObject.Properties | ForEach-Object {
+        Write-Host "Removing $($_.Name)/$($_.Value.version)..."
+        Remove-Pack -Id $_.Name -Version $_.Value.version -Kind $_.Value.kind
     }
 }
 
