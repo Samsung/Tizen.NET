0ebd9f586c994a18b2ce0f7d71171133cb2c8ddb
sung-su.kim <sung-su.kim@samsung.com>
2022-07-20 14:34:03 +0900

    Update install script to add new option (#194)
    LGTM

--- workload/scripts/workload-install.sh
+++ workload/scripts/workload-install.sh
@@ -6,12 +6,21 @@
 #!/bin/bash -e
 
 MANIFEST_BASE_NAME="samsung.net.sdk.tizen.manifest"
-SUPPORTED_DOTNET_VERSION="6"
 MANIFEST_VERSION="<latest>"
 DOTNET_INSTALL_DIR="<auto>"
 DOTNET_TARGET_VERSION_BAND="<auto>"
 DOTNET_DEFAULT_PATH_LINUX="/usr/share/dotnet"
 DOTNET_DEFAULT_PATH_MACOS="/usr/local/share/dotnet"
+UPDATE_ALL_WORKLOADS="false"
+
+LatestVersionMap=(
+    "$MANIFEST_BASE_NAME-6.0.100=6.5.100-rc.1.120"
+    "$MANIFEST_BASE_NAME-6.0.200=7.0.100-preview.13.6"
+    "$MANIFEST_BASE_NAME-6.0.300=7.0.304"
+    "$MANIFEST_BASE_NAME-6.0.400=7.0.400-preview.2.6"
+    "$MANIFEST_BASE_NAME-7.0.100-preview.6=7.0.100-preview.6.14"
+    "$MANIFEST_BASE_NAME-7.0.100-preview.7=7.0.100-preview.7.20"
+    )
 
 while [ $# -ne 0 ]; do
     name=$1
@@ -27,6 +36,10 @@ while [ $# -ne 0 ]; do
         -t|--dotnet-target-version-band)
             shift
             DOTNET_TARGET_VERSION_BAND=$1
+            ;;
+        -u|--update-all-workloads)
+            shift
+            UPDATE_ALL_WORKLOADS="true"
             ;;
         -h|--help)
             script_name="$(basename "$0")"
@@ -90,6 +103,14 @@ if [ ! -d $DOTNET_INSTALL_DIR ]; then
     exit 1
 fi
 
+function getLatestVersion () {
+    for index in "${LatestVersionMap[@]}"; do
+         if [ "${index%%=*}" = "${1}" ]; then
+             echo "${index#*=}"
+         fi
+    done
+}
+
 # Check installed dotnet version
 DOTNET_COMMAND="$DOTNET_INSTALL_DIR/dotnet"
 
@@ -98,94 +119,96 @@ if [ ! -x "$DOTNET_COMMAND" ]; then
     exit 1
 fi
 
-DOTNET_VERSION=$($DOTNET_COMMAND --version)
-IFS='.' read -r -a array <<< "$DOTNET_VERSION"
+function install_tizenworkload() {
+    DOTNET_VERSION=$1
+    IFS='.' read -r -a array <<< "$DOTNET_VERSION"
+    CURRENT_DOTNET_VERSION=${array[0]}
+    DOTNET_VERSION_BAND="${array[0]}.${array[1]}.${array[2]:0:1}00"
+    MANIFEST_NAME="$MANIFEST_BASE_NAME-$DOTNET_VERSION_BAND"
 
-CURRENT_DOTNET_VERSION=${array[0]}
-if [[ $CURRENT_DOTNET_VERSION -lt $SUPPORTED_DOTNET_VERSION ]]; then
-    echo "Current .NET version is $CURRENT_DOTNET_VERSION. .NET SDK version $SUPPORTED_DOTNET_VERSION or later is required."
-    exit 0
-fi
+    # Reset local variables
+    if [[ "$UPDATE_ALL_WORKLOADS" == "true" ]]; then
+        DOTNET_TARGET_VERSION_BAND="<auto>"
+        MANIFEST_VERSION="<latest>"
+    fi
 
-DOTNET_VERSION_BAND="${array[0]}.${array[1]}.${array[2]:0:1}00"
-MANIFEST_NAME="$MANIFEST_BASE_NAME-$DOTNET_VERSION_BAND"
-
-if [[ "$DOTNET_TARGET_VERSION_BAND" == "<auto>" ]]; then
-    if [[ "$CURRENT_DOTNET_VERSION" -ge "7" ]]; then
-        if [[ "$DOTNET_VERSION" == *"-preview"* || $DOTNET_VERSION == *"-rc"* || $DOTNET_VERSION == *"-alpha"* ]] && [[ ${#array[@]} -ge 4 ]]; then
-            DOTNET_TARGET_VERSION_BAND="$DOTNET_VERSION_BAND${array[2]:3}.${array[3]}"
-            MANIFEST_NAME="$MANIFEST_BASE_NAME-$DOTNET_TARGET_VERSION_BAND"
+    # Check version band
+    if [[ "$DOTNET_TARGET_VERSION_BAND" == "<auto>" ]]; then
+        if [[ "$CURRENT_DOTNET_VERSION" -ge "7" ]]; then
+            if [[ "$DOTNET_VERSION" == *"-preview"* || $DOTNET_VERSION == *"-rc"* || $DOTNET_VERSION == *"-alpha"* ]] && [[ ${#array[@]} -ge 4 ]]; then
+                DOTNET_TARGET_VERSION_BAND="$DOTNET_VERSION_BAND${array[2]:3}.${array[3]}"
+                MANIFEST_NAME="$MANIFEST_BASE_NAME-$DOTNET_TARGET_VERSION_BAND"
+            else
+                DOTNET_TARGET_VERSION_BAND=$DOTNET_VERSION_BAND
+            fi
         else
             DOTNET_TARGET_VERSION_BAND=$DOTNET_VERSION_BAND
         fi
-    else
-        DOTNET_TARGET_VERSION_BAND=$DOTNET_VERSION_BAND
     fi
-fi
 
-LatestVersionMap=(
-    "$MANIFEST_BASE_NAME-6.0.100=6.5.100-rc.1.120"
-    "$MANIFEST_BASE_NAME-6.0.200=7.0.100-preview.13.6"
-    "$MANIFEST_BASE_NAME-6.0.300=7.0.303"
-    "$MANIFEST_BASE_NAME-6.0.400=7.0.400-preview.2.6"
-    "$MANIFEST_BASE_NAME-7.0.100-preview.6=7.0.100-preview.6.14"
-    "$MANIFEST_BASE_NAME-7.0.100-preview.7=7.0.100-preview.7.20"
-    )
-
-function getLatestVersion () {
-    for index in "${LatestVersionMap[@]}"; do
-         if [ "${index%%=*}" = "${1}" ]; then
-             echo "${index#*=}"
-         fi
-    done
-    echo ""
-}
-
-# Check latest version of manifest.
-if [[ "$MANIFEST_VERSION" == "<latest>" ]]; then
-    MANIFEST_VERSION=$(curl -s https://api.nuget.org/v3-flatcontainer/$MANIFEST_NAME/index.json | grep \" | tail -n 1 | tr -d '\r' | xargs)
-    if [ ! "$MANIFEST_VERSION" ]; then
-        MANIFEST_VERSION=$(getLatestVersion "$MANIFEST_NAME")
-        if [[ -n $MANIFEST_VERSION ]]; then
-            echo "Return cached latest version: $MANIFEST_VERSION"
-        else
-            echo "Failed to get the latest version of $MANIFEST_NAME."
-            exit 1
+    # Check latest version of manifest.
+    if [[ "$MANIFEST_VERSION" == "<latest>" ]]; then
+        MANIFEST_VERSION=$(curl -s https://api.nuget.org/v3-flatcontainer/$MANIFEST_NAME/index.json | grep \" | tail -n 1 | tr -d '\r' | xargs)
+        if [ ! "$MANIFEST_VERSION" ]; then
+            MANIFEST_VERSION=$(getLatestVersion "$MANIFEST_NAME")
+            if [[ -n $MANIFEST_VERSION ]]; then
+                echo "Return cached latest version: $MANIFEST_VERSION"
+            else
+                echo "Failed to get the latest version of $MANIFEST_NAME."
+                return
+            fi
         fi
     fi
+
+    # Check workload manifest directory.
+    SDK_MANIFESTS_DIR=$DOTNET_INSTALL_DIR/sdk-manifests/$DOTNET_TARGET_VERSION_BAND
+    ensure_directory $SDK_MANIFESTS_DIR
+
+    TMPDIR=$(mktemp -d)
+
+    echo "Installing $MANIFEST_NAME/$MANIFEST_VERSION to $SDK_MANIFESTS_DIR..."
+
+    # Download and extract the manifest nuget package.
+    curl -s -o $TMPDIR/manifest.zip -L https://www.nuget.org/api/v2/package/$MANIFEST_NAME/$MANIFEST_VERSION
+
+    unzip -qq -d $TMPDIR/unzipped $TMPDIR/manifest.zip
+    if [ ! -d $TMPDIR/unzipped/data ]; then
+        echo "No such files to install."
+        return
+    fi
+    chmod 744 $TMPDIR/unzipped/data/*
+
+    # Copy manifest files to dotnet sdk.
+    mkdir -p $SDK_MANIFESTS_DIR/samsung.net.sdk.tizen
+    cp -f $TMPDIR/unzipped/data/* $SDK_MANIFESTS_DIR/samsung.net.sdk.tizen/
+
+    if [ ! -f $SDK_MANIFESTS_DIR/samsung.net.sdk.tizen/WorkloadManifest.json ]; then
+        echo "Installation is failed."
+        return
+    fi
+
+    # Install workload packs.
+    $DOTNET_INSTALL_DIR/dotnet workload install tizen --skip-manifest-update
+
+    # Clean-up
+    rm -fr $TMPDIR
+
+    echo "Done installing Tizen workload $MANIFEST_VERSION"
+}
+
+if [[ "$UPDATE_ALL_WORKLOADS" == "true" ]]; then
+    INSTALLED_DOTNET_SDKS=$($DOTNET_COMMAND --list-sdks | sed -n '/^6\|^7/p' | sed 's/ \[.*//g')
+else
+    INSTALLED_DOTNET_SDKS=$($DOTNET_COMMAND --version)
 fi
 
-# Check workload manifest directory.
-SDK_MANIFESTS_DIR=$DOTNET_INSTALL_DIR/sdk-manifests/$DOTNET_TARGET_VERSION_BAND
-
-ensure_directory $SDK_MANIFESTS_DIR
-
-TMPDIR=$(mktemp -d)
-
-echo "Installing $MANIFEST_NAME/$MANIFEST_VERSION to $SDK_MANIFESTS_DIR..."
-
-# Download and extract the manifest nuget package.
-curl -s -o $TMPDIR/manifest.zip -L https://www.nuget.org/api/v2/package/$MANIFEST_NAME/$MANIFEST_VERSION
-unzip -qq -d $TMPDIR/unzipped $TMPDIR/manifest.zip
-if [ ! -d $TMPDIR/unzipped/data ]; then
-    echo "No such files to install."
-    exit 1
-fi
-chmod 744 $TMPDIR/unzipped/data/*
-
-# Copy manifest files to dotnet sdk.
-mkdir -p $SDK_MANIFESTS_DIR/samsung.net.sdk.tizen
-cp -f $TMPDIR/unzipped/data/* $SDK_MANIFESTS_DIR/samsung.net.sdk.tizen/
-
-if [ ! -f $SDK_MANIFESTS_DIR/samsung.net.sdk.tizen/WorkloadManifest.json ]; then
-    echo "Installation is failed."
-    exit 1
+if [ -z "$INSTALLED_DOTNET_SDKS" ]; then
+    echo ".NET SDK version 6 or later is required to install Tizen Workload."
+else
+    for DOTNET_SDK in $INSTALLED_DOTNET_SDKS; do
+        echo "Check Tizen Workload for sdk $DOTNET_SDK."
+        install_tizenworkload $DOTNET_SDK
+    done
 fi
 
-# Install workload packs.
-$DOTNET_INSTALL_DIR/dotnet workload install tizen --skip-manifest-update
-
-# Clean-up
-rm -fr $TMPDIR
-
-echo "Done installing Tizen workload $MANIFEST_VERSION"
+echo "DONE"
