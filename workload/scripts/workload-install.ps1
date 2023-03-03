165fd51f7be01a142fe708de734b37a53c0ababf
sung-su.kim <sung-su.kim@samsung.com>
2023-03-03 14:11:46 +0900

    Update main (#234)

--- workload/scripts/workload-install.ps1
+++ workload/scripts/workload-install.ps1
@@ -40,6 +40,7 @@ $LatestVersionMap = @{
     "$ManifestBaseName-7.0.100" = "7.0.103";
     "$ManifestBaseName-7.0.200" = "7.0.105";
     "$ManifestBaseName-8.0.100-alpha.1" = "7.0.104";
+    "$ManifestBaseName-8.0.100-preview.2" = "7.0.106";
 }
 
 function New-TemporaryDirectory {
