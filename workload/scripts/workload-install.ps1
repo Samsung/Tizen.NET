e49719a01eb26c36020c1fa954173b2d6b1b60a5
Jay Cho <chojoong@gmail.com>
2023-06-13 15:46:19 +0900

    Fix KnownFrameworkReference

--- workload/scripts/workload-install.ps1
+++ workload/scripts/workload-install.ps1
@@ -32,20 +32,20 @@ $LatestVersionMap = @{
     "$ManifestBaseName-6.0.100" = "7.0.101";
     "$ManifestBaseName-6.0.200" = "7.0.100-preview.13.6";
     "$ManifestBaseName-6.0.300" = "7.0.304";
-    "$ManifestBaseName-6.0.400" = "7.0.116";
+    "$ManifestBaseName-6.0.400" = "7.0.119";
     "$ManifestBaseName-7.0.100-preview.6" = "7.0.100-preview.6.14";
     "$ManifestBaseName-7.0.100-preview.7" = "7.0.100-preview.7.20";
     "$ManifestBaseName-7.0.100-rc.1" = "7.0.100-rc.1.22";
     "$ManifestBaseName-7.0.100-rc.2" = "7.0.100-rc.2.24";
     "$ManifestBaseName-7.0.100" = "7.0.103";
     "$ManifestBaseName-7.0.200" = "7.0.105";
-    "$ManifestBaseName-7.0.300" = "7.0.117";
+    "$ManifestBaseName-7.0.300" = "7.0.120";
     "$ManifestBaseName-8.0.100-alpha.1" = "7.0.104";
     "$ManifestBaseName-8.0.100-preview.2" = "7.0.106";
     "$ManifestBaseName-8.0.100-preview.3" = "7.0.107";
     "$ManifestBaseName-8.0.100-preview.4" = "7.0.108";
     "$ManifestBaseName-8.0.100-preview.5" = "7.0.110";
-    "$ManifestBaseName-8.0.100-preview.6" = "7.0.112";
+    "$ManifestBaseName-8.0.100-preview.6" = "7.0.121";
 }
 
 function New-TemporaryDirectory {
