6201abd8c59b89866ba97de82d50640f9ae0624e
sung-su.kim <sung-su.kim@samsung.com>
2024-02-27 15:25:09 +0900

    Update version

--- workload/scripts/workload-install.ps1
+++ workload/scripts/workload-install.ps1
@@ -32,7 +32,7 @@ $LatestVersionMap = @{
     "$ManifestBaseName-6.0.100" = "7.0.101";
     "$ManifestBaseName-6.0.200" = "7.0.100-preview.13.6";
     "$ManifestBaseName-6.0.300" = "7.0.304";
-    "$ManifestBaseName-6.0.400" = "7.0.119";
+    "$ManifestBaseName-6.0.400" = "8.0.140";
     "$ManifestBaseName-7.0.100-preview.6" = "7.0.100-preview.6.14";
     "$ManifestBaseName-7.0.100-preview.7" = "7.0.100-preview.7.20";
     "$ManifestBaseName-7.0.100-rc.1" = "7.0.100-rc.1.22";
@@ -40,7 +40,7 @@ $LatestVersionMap = @{
     "$ManifestBaseName-7.0.100" = "7.0.103";
     "$ManifestBaseName-7.0.200" = "7.0.105";
     "$ManifestBaseName-7.0.300" = "7.0.120";
-    "$ManifestBaseName-7.0.400" = "7.0.123";
+    "$ManifestBaseName-7.0.400" = "8.0.141";
     "$ManifestBaseName-8.0.100-alpha.1" = "7.0.104";
     "$ManifestBaseName-8.0.100-preview.2" = "7.0.106";
     "$ManifestBaseName-8.0.100-preview.3" = "7.0.107";
@@ -52,7 +52,7 @@ $LatestVersionMap = @{
     "$ManifestBaseName-8.0.100-rc.2" = "7.0.125";
     "$ManifestBaseName-8.0.100-rtm" = "7.0.127";
     "$ManifestBaseName-8.0.100" = "8.0.130";
-    "$ManifestBaseName-8.0.200" = "8.0.138";
+    "$ManifestBaseName-8.0.200" = "8.0.142";
 }
 
 function New-TemporaryDirectory {
