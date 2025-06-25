10c6fbe3b4b26994dc8265a74174c352c0fd9d28
Jay Cho <chojoong@gmail.com>
2025-06-25 19:20:41 +0900

    Update version with fixes

--- workload/scripts/workload-install.ps1
+++ workload/scripts/workload-install.ps1
@@ -54,7 +54,7 @@ $LatestVersionMap = @{
     "$ManifestBaseName-8.0.100" = "8.0.144";
     "$ManifestBaseName-8.0.200" = "8.0.145";
     "$ManifestBaseName-8.0.300" = "8.0.149";
-    "$ManifestBaseName-8.0.400" = "10.0.103";
+    "$ManifestBaseName-8.0.400" = "10.0.107";
 }
 
 function New-TemporaryDirectory {
--- workload/scripts/workload-install.sh
+++ workload/scripts/workload-install.sh
@@ -39,7 +39,7 @@ LatestVersionMap=(
     "$MANIFEST_BASE_NAME-8.0.100=8.0.144"
     "$MANIFEST_BASE_NAME-8.0.200=8.0.145"
     "$MANIFEST_BASE_NAME-8.0.300=8.0.149"
-    "$MANIFEST_BASE_NAME-8.0.400=10.0.103"
+    "$MANIFEST_BASE_NAME-8.0.400=10.0.107"
     )
 
 while [ $# -ne 0 ]; do
